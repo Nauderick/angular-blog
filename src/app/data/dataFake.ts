adeb0a924e7976a5c8dc80e11ea28d72d8c1a08e
Nauderick <103642470+Nauderick@users.noreply.github.com>
2022-10-21 19:03:25 -0300

    prototype done

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Dragon Quest XI",
+        "description": "Lançamento do novo Dragon Quest XI",
+        "photoCover": "https://th.bing.com/th/id/R.4a3cbb95d24cf853d804bb4709580f83?rik=7YjdXY94pUpI4g&pid=ImgRaw&r=0",
+    },
+    {
+        "id":"2",
+        "title": "Lançamento Dragon Quest I",
+        "description": "description default",
+        "photoCover": "https://th.bing.com/th/id/R.95555eddff23564a80aa4d108062f69b?rik=0M3Xn3hLlJLO5g&pid=ImgRaw&r=0",
+    },
+    {
+        "id":"3",
+        "title": "Lançamento Dragon Quest II",
+        "description": "description default",
+        "photoCover": "https://th.bing.com/th/id/R.f94f6f7425c6e8c8e15cfb4c3bb40468?rik=4W5R655PxGW6MQ&pid=ImgRaw&r=0",
+    },
+    {
+        "id":"4",
+        "title": "Lançamento Dragon Quest XII",
+        "description": "description default",
+        "photoCover": "https://cdn.vox-cdn.com/thumbor/b5-duEvrZeYK_psAPcSHan9iWBk=/62x0:737x450/1200x800/filters:focal(62x0:737x450)/cdn.vox-cdn.com/uploads/chorus_image/image/47651187/dragon_quest_7.0.0.jpg",
+    }
+]
